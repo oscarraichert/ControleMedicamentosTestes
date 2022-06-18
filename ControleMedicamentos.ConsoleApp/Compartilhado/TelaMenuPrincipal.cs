a6477deb2d2729b89bdf2ec3a5be3a767615e3f9
oscarraichert <oscarraichert@hotmail.com>
2022-06-18 00:02:54 -0300

    Add project files.

---
+++ ControleMedicamentos.ConsoleApp/Compartilhado/TelaMenuPrincipal.cs
@@ -0,0 +1,58 @@
+﻿using ControleMedicamentos.ConsoleApp.ModuloFornecedor;
+using ControleMedicamentos.ConsoleApp.ModuloFuncionario;
+using System;
+
+namespace ControleMedicamentos.ConsoleApp.Compartilhado
+{
+    public class TelaMenuPrincipal
+    {
+        private RepositorioFuncionario repositorioFuncionario;
+        private TelaCadastroFuncionario telaCadastroFuncionario;
+
+        private RepositorioFornecedor repositorioFornecedor;
+        private TelaCadastroFornecedor telaCadastroFornecedor;
+
+        public TelaMenuPrincipal(Notificador notificador)
+        {
+            repositorioFuncionario = new RepositorioFuncionario();
+            telaCadastroFuncionario = new TelaCadastroFuncionario(repositorioFuncionario, notificador);
+
+            repositorioFornecedor = new RepositorioFornecedor();
+            telaCadastroFornecedor = new TelaCadastroFornecedor(repositorioFornecedor, notificador);
+        }
+
+        public string MostrarOpcoes()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Controle de Retirada de Medicamentos 1.0");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Digite 1 para Gerenciar Funcionários");
+            Console.WriteLine("Digite 2 para Gerenciar Fornecedores");
+
+
+            Console.WriteLine("Digite s para sair");
+
+            string opcaoSelecionada = Console.ReadLine();
+
+            return opcaoSelecionada;
+        }
+
+        public TelaBase ObterTela()
+        {
+            string opcao = MostrarOpcoes();
+
+            TelaBase tela = null;
+
+            if (opcao == "1")
+                tela = telaCadastroFuncionario;
+
+            else if (opcao == "2")
+                tela = telaCadastroFornecedor;
+
+            return tela;
+        }
+    }
+}
---
+++ ControleMedicamentos.ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using System;
+
+namespace ControleMedicamentos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notificador notificador = new Notificador();
+            TelaMenuPrincipal menuPrincipal = new TelaMenuPrincipal(notificador);
+
+            while (true)
+            {
+                TelaBase telaSelecionada = menuPrincipal.ObterTela();
+
+                if (telaSelecionada is null)
+                    return;
+
+                string opcaoSelecionada = telaSelecionada.MostrarOpcoes();
+
+                if (telaSelecionada is ITelaCadastravel)
+                {
+                    ITelaCadastravel telaCadastravel = (ITelaCadastravel)telaSelecionada;
+
+                    if (opcaoSelecionada == "1")
+                        telaCadastravel.Inserir();
+
+                    else if (opcaoSelecionada == "2")
+                        telaCadastravel.Editar();
+
+                    else if (opcaoSelecionada == "3")
+                        telaCadastravel.Excluir();
+
+                    else if (opcaoSelecionada == "4")
+                        telaCadastravel.VisualizarRegistros("Tela");
+                }
+            }
+        }
+    }
+}
